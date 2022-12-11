50f5cc17e1d2f5827b94b2f69d3ca736fb69ac04
Benji <64211264+GrumpyBenji@users.noreply.github.com>
2022-12-11 09:15:12 -0800

    Solved All

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,24 @@
 // Your code here
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...args){
+    let dragonNames = [];
+    args.forEach(element => {
+      if(element instanceof Dragon){
+        dragonNames.push(element.name);
+      }
+    })
+    return dragonNames;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
